baafe7be1bd17933d3b10c8a4444677324434f96
luciomelis <luciomelis1992@gmail.com>
2022-05-18 15:21:49 +0200

    ho inserito l'html di header e footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,115 @@
+<footer>
+    <!-- ********footer top**********  -->
+    <div class="footer-top">
+        <!-- container footer-top  -->
+        <div class="container-footer-top">
+            <!-- container singola card  -->
+            <div class="container-card">
+                <img src="../assets/buy-comics-digital-comics.png" alt="digi-comics" />
+                <p>DIGITAL COMICS</p>
+            </div>
+            <div class="container-card">
+                <img src="../assets/buy-comics-merchandise.png" alt="merchandise" />
+                <p>DC MERCHANDISE</p>
+            </div>
+            <div class="container-card">
+                <img src="../assets/buy-comics-subscriptions.png" alt="subscriptions" />
+                <p>SUBSCRIPTION</p>
+            </div>
+            <div class="container-card">
+                <img src="../assets/buy-comics-shop-locator.png" alt="locator" />
+                <p>COMIC SHOP LOCATOR</p>
+            </div>
+            <div class="container-card">
+                <img src="../assets/buy-dc-power-visa.svg" alt="power-visa" />
+                <p>DC POWER VISA</p>
+            </div>
+        </div>
+    </div>
+    <!-- ***********footer center***********  -->
+    <div class="footer-center">
+        <!-- container general footer center  -->
+        <div class="container-footer-center">
+            <!-- ***container List*** -->
+            <div class="container-list">
+                <!-- prima lista  -->
+                <div>
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <!-- seconda lista  -->
+                <div>
+                    <h4>DC</h4>
+                    <ul>
+                        <li>Terms of Use</li>
+                        <li>Privacy policy(New)</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <!-- terza lista  -->
+                <div>
+                    <h4>SITES</h4>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+            <!-- ***container background*** -->
+            <div class="container-background"></div>
+        </div>
+    </div>
+    <!-- ***********footer bottom***********  -->
+    <div class="footer-bottom">
+        <!-- container-footer-bottom  -->
+        <div class="container-footer-bottom">
+            <!-- container button  -->
+            <div class="button-footer">
+                <button>SIGN-UP NOW</button>
+            </div>
+            <!-- container img  -->
+            <div class="follow-footer">
+                <h4>FOLLOW US</h4>
+                <div>
+                    <img src="../assets/footer-facebook.png" alt="Facebook" />
+                </div>
+                <div>
+                    <img src="../assets/footer-twitter.png" alt="Twitter" />
+                </div>
+                <div>
+                    <img src="../assets/footer-youtube.png" alt="YouTube" />
+                </div>
+                <div>
+                    <img src="../assets/footer-pinterest.png" alt="Pinterest" />
+                </div>
+                <div>
+                    <img src="../assets/footer-periscope.png" alt="Periscope" />
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="container-header">
+        <div class="container-header-logo">
+            <img src="../assets/dc-logo.png" alt="Dc-Logo" />
+        </div>
+        <div class="container-nav">
+            <ul>
+                <li><a href="#">COMICS</a></li>
+                <li><a href="#">MOVIES</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">GAMES</a></li>
+                <li><a href="#">COLLECTIBLES</a></li>
+                <li><a href="#">VIDEO</a></li>
+                <li><a href="#">FANS</a></li>
+                <li><a href="#">NEWS</a></li>
+                <li><a href="#">SHOP</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
