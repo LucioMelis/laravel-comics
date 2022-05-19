8c8c1b9d60b23130108b689e89098872cf37b685
luciomelis <luciomelis1992@gmail.com>
2022-05-19 19:02:28 +0200

    esercizio terminato

--- resources/views/partials/mainCard.blade.php
+++ resources/views/partials/mainCard.blade.php
@@ -1,4 +1,4 @@
-<main>
+<main class="descriptionCard">
     <!-- container-Jumbotron  -->
     <div class="container-bg_main-top"></div>
     <!-- container Comics  -->
@@ -7,19 +7,70 @@
             <img src="{{ $comics['thumb'] }}" alt="ciao">
         </div>
     </div>
-    <div class="container-bg-main-bottom-singleCard">
-        <div class="content-dc-singleCard">
-            <div class="comic-description">
-                <h1>{{ $comics['title'] }}</h1>
-                <div class="banner-price">
-                    <p>U.S. Price: <span>{{ $comics['price'] }}</span></p>
-                    <div>
-                        <p>AVAILABLE</p>
-                        <span class="check">Check Availability &#9662;</span>
-                    </div>
+    <div class="container">
+        <div class="description">
+            <h2>{{ strtoupper($comics['title']) }}</h2>
+            <div class="green-banner">
+                <div class="status">
+                    <p>
+                        <span>U.S. Price: </span>
+                        {{ $comics['price'] }}
+                    </p>
+                    <div>AVAILABLE</div>
+                </div>
+                <div class="check-status">Check Availability &#9662;</div>
+            </div>
+            <p class="description">{{ $comics['description'] }}</p>
+        </div>
+        <div class="advertisement">
+            <h5>ADVERTISEMENT</h5>
+            <img src="/images/advertise.jpg" alt="advertisement">
+        </div>
+    </div>
+    <div class="more-infos">
+        <div class="container">
+            <div class="talent">
+                <h3>Talent</h3>
+                <div class="info">
+                    <h5>Art by:</h5>
+                    <p>
+                        @foreach ($comics['artists'] as $artist)
+                            <a href="#">
+                                {{ $artist }}
+                            </a>
+                        @endforeach
+                        {{-- {{ $card['artists'] }}</p> --}}
+                </div>
+                <div class="info">
+                    <h5>Written by:</h5>
+                    <p>
+                        @foreach ($comics['writers'] as $writer)
+                            <a href="#">
+                                {{ $writer }}
+                            </a>
+                        @endforeach
+                    </p>
                 </div>
             </div>
-            <div class="advertisement">ciao2</div>
+            <div class="specs">
+                <h3>Specs</h3>
+                <div class="info">
+                    <h5>Series:</h5>
+                    <p>
+                        <a href="#">
+                            {{ strtoupper($comics['series']) }}
+                        </a>
+                    </p>
+                </div>
+                <div class="info">
+                    <h5>U.S. Price:</h5>
+                    <p>{{ $comics['price'] }}</p>
+                </div>
+                <div class="info">
+                    <h5>On Sale Date:</h5>
+                    <p>{{ $comics['sale_date'] }}</p>
+                </div>
+            </div>
         </div>
     </div>
 </main>
